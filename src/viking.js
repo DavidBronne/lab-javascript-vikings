5f8e834da7e588fe2016d4234417a79b0d948bcf
David Bronne <davidbronne@gmail.com>
2020-01-17 17:59:08 +0100

    Initial commit

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,35 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+    this.health = health;
+    this.strength = strength;
+    this.attack = function (){
+        return this.strength;
+    };
+    this.receiveDamage =function (damage) {
+        this.health -= damage; 
+    }
+}
 
 // Viking
-function Viking() {}
+function Viking(health, strength, name) {
+    Soldier.call(this,health, strength);
+    this.name = name;
+    this.receiveDamage =function (damage) {
+        this.health -= damage;
+        if(this.health > 0) {
+            return `${this.name} has received ${damage} points of damge`
+        } else {
+            return `${this.name} has died in act of combat`
+        }
+    }
+    this.battleCry = function () {
+        return "Odin Owns You All!"
+    }
+
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
 
 // Saxon
 function Saxon() {}
